f8f434a8d60cd25ecc9fc977bdf9e9309356b63a
SuJiaKuan <feabries@gmail.com>
2020-02-26 23:35:51 +0800

    Refactor qa_eval/visualize.py for common used code

--- qa_eval/visualize.py
+++ qa_eval/visualize.py
@@ -2,38 +2,52 @@ import pandas as pd
 from tabulate import tabulate
 
 
-def visualize_correctness(correctness_list, save_path=None):
-    # Convert correctness list to DataFrame to make visualization and file
-    # saving easier.
-    correctness_df = pd.DataFrame({
-        "Type": [c["type"] for c in correctness_list],
-        "Question Count": [c["question_count"] for c in correctness_list],
-        "Correct Count": [c["correct_count"] for c in correctness_list],
-        "Correct Rate": [c["correct_rate"] for c in correctness_list],
+CORRECTNESS_HEAD_KEY_LIST = [
+    ("Type", "type"),
+    ("Question Count", "question_count"),
+    ("Correct Count", "correct_count"),
+    ("Correct Rate", "correct_rate"),
+]
+
+ERROR_REASONS_HEAD_KEY_LIST = [
+    ("Reason", "reason"),
+    ("Count", "count"),
+    ("Rate", "rate"),
+]
+
+
+def _list_to_df(source_list, head_key_list):
+    # Convert source list to DataFrame to make visualization and file saving
+    # easier.
+    df = pd.DataFrame({
+        head: [s[key] for s in source_list]
+        for head, key in head_key_list
     })
 
-    print("====================== Correctness Result ======================")
-    print(tabulate(correctness_df, headers='keys', tablefmt='psql'))
+    return df
 
-    # Save result to a csv file.
+
+def _df_to_stdout(df, theme):
+    print(
+        "====================== {} Result ======================".format(theme)
+    )
+    print(tabulate(df, headers='keys', tablefmt='psql'))
+
+
+def _df_to_csv(df, save_path):
+    # Save result to a csv file if path is provided.
     if save_path is not None:
-        correctness_df.to_csv(save_path)
-        print("Correctness result saved to {}".format(save_path))
+        df.to_csv(save_path)
+        print("Result saved to {}".format(save_path))
+
+
+def visualize_correctness(correctness_list, save_path=None):
+    correctness_df = _list_to_df(correctness_list,CORRECTNESS_HEAD_KEY_LIST)
+    _df_to_stdout(correctness_df, "Correctness")
+    _df_to_csv(correctness_df, save_path)
 
 
 def visualize_error_reasons(error_reasons, save_path=None):
-    # Convert error rates to DataFrame to make visualization and file
-    # saving easier.
-    error_reasons_df = pd.DataFrame({
-        "Reason": [r["reason"] for r in error_reasons],
-        "Count": [r["count"] for r in error_reasons],
-        "Rate": [r["rate"] for r in error_reasons],
-    })
-
-    print("====================== Error Reasons Result ======================")
-    print(tabulate(error_reasons_df, headers='keys', tablefmt='psql'))
-
-    # Save result to a csv file.
-    if save_path is not None:
-        error_reasons_df.to_csv(save_path)
-        print("Error reasons result saved to {}".format(save_path))
+    error_reasons_df = _list_to_df(error_reasons, ERROR_REASONS_HEAD_KEY_LIST)
+    _df_to_stdout(error_reasons_df, "Error Reasons")
+    _df_to_csv(error_reasons_df, save_path)
